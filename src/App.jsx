0f7e279305a0ebbbb09726119d611bae2b9ce240
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-18 16:02:17 -0700

    created App.jsx framework

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// src/App.js
+import React, { useState } from 'react';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import './App.css';
+
+const App = () => {
+  const [activeSection, setActiveSection] = useState('AboutMe');
+
+  const renderSection = () => {
+    switch (activeSection) {
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <AboutMe />;
+    }
+  };
+
+  return (
+    <div>
+      <Header activeSection={activeSection} setActiveSection={setActiveSection} />
+      <main>{renderSection()}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
