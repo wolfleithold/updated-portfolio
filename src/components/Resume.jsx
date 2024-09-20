db6fb3d594b148e61a42fac5ee8e69398dca2cf8
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-19 22:45:09 -0700

    updating resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import '../App.css';
+import resume from '../assets/Wolfgang_Leithold_Resume.pdf';
 
 const Resume = () => {
   return (
     <section className="resume">
-      <h2>Resume</h2>
-      <a href="/path-to-resume.pdf" download>Download Resume</a>
+      <h2>My Resume</h2>
+      <embed src={resume} width="50%" height="500px" type="application/pdf" />
       <h3>Proficiencies</h3>
       <ul>
         <li>JavaScript</li>
         <li>React</li>
         <li>Node.js</li>
         <li>CSS</li>
-        {/* Add other proficiencies */}
+        <li>HTML</li>
       </ul>
     </section>
   );
